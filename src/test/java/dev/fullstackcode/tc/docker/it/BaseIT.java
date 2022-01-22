7a4688da9ffbdf048b93634745228dfc5314947c
sureshgadupu <sureshgkhyd@gmail.com>
2022-01-22 19:41:33 +1300

    code cleanup

--- src/test/java/dev/fullstackcode/tc/docker/it/BaseIT.java
+++ src/test/java/dev/fullstackcode/tc/docker/it/BaseIT.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-//classes = {PostgresContainerConfiguration.class}
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public  class BaseIT {
